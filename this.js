8f9d878d9bf9cb9c63586f8479292377d8857669
Asterrr1 <SquigglyWiggly03@gmail.com>
2023-04-24 19:45:28 -0400

    commit

---
+++ this.js
@@ -0,0 +1,8 @@
+
+let openCreatePostPrompt = () => {
+    document.getElementById("NewPost").style.display = "flex";
+};
+
+let closeCreatePostPrompt = () => {
+    document.getElementById("NewPost").style.display = "none";
+};
